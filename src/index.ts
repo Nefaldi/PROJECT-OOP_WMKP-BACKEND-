bac418689bd1338a7f2f129893108b50a9706258
TrafaIgar <nefalnamaku@gmail.com>
2026-06-01 01:02:04 +0800

    fix: vercel json

--- src/index.ts
+++ src/index.ts
@@ -140,6 +140,10 @@ app.put('/api/orders/:id/status', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+export default app;
